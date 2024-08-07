b258dbcf2bdc81303c3700c16f5270d795c76419
marufkhan09 <maruf1609@gmail.com>
2024-08-07 19:01:29 +0600

    Initial Commit

---
+++ FirstRowSwiftUI/FirstRowSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstRowSwiftUIApp.swift
+//  FirstRowSwiftUI
+//
+//  Created by Maruf Khan on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstRowSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
