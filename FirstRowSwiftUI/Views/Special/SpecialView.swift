37fa738f08f0ea055fc0c2219d6f5ec04f643244
marufkhan09 <maruf1609@gmail.com>
2024-08-08 17:35:48 +0600

    Special nav added

--- FirstRowSwiftUI/Views/Special/SpecialView.swift
+++ FirstRowSwiftUI/Views/Special/SpecialView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct SpecialView: View {
+    @EnvironmentObject var settings: AppSettings
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            GeometryReader { geo in
+                ScrollView {
+                    VStack {
+                        ZStack(alignment:.bottom) {
+                            AssetNames.Colors.appbarColor
+                                .frame(width: geo.size.width, height: 50)
+                            
+                            // Black background with uneven corners
+                            Color.black
+                                .frame(width: geo.size.width, height: 40)
+                                .clipShape(UnevenRoundedRectangle(topLeadingRadius: 15, topTrailingRadius: 15))
+                            
+                            Text("Fantasy").foregroundStyle(.white).padding(.bottom,8).font(.title2)
+                        }
+                    }
+                }.toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Image(AssetNames.Images.statusBarLogo)
+                    }
+                }
+                
+                .navigationBarModifier(
+                    backgroundColor: UIColor(red: 0.114, green: 0.188, blue: 0.322, alpha: 1.0),
+                    foregroundColor: UIColor.white,
+                    tintColor: nil,
+                    withSeparator: false
+                )
+            }
+        }
     }
 }
 
 #Preview {
-    SpecialView()
+    SpecialView().environmentObject(AppSettings())
 }
