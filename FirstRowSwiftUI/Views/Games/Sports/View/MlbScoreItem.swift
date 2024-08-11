2681f4698818172e9722d7141813fa10e393f7d2
marufkhan09 <maruf1609@gmail.com>
2024-08-12 03:22:27 +0600

    mlb item initial ui done

--- FirstRowSwiftUI/Views/Games/Sports/View/MlbScoreItem.swift
+++ FirstRowSwiftUI/Views/Games/Sports/View/MlbScoreItem.swift
@@ -1,5 +1,5 @@
 //
-//  MlbScoreItem.swift
+//  ScoreItem.swift
 //  FirstRowSwiftUI
 //
 //  Created by Maruf Khan on 11/8/24.
@@ -9,53 +9,48 @@ import SwiftUI
 
 struct MlbScoreItem: View {
     let item: MlbData
-    
     var body: some View {
-        VStack {
-            HStack {
-                VStack {
-                    
-                    // Team Details View
-                    TeamView(team: item.teamsNormalized?.first, score: item.score?.scoreAway, line: item.lines?.first, isHome: false)
-                    
+        GeometryReader { proxy in
+          
+            VStack (alignment:.center){
+                HStack(alignment:.center){
+                        VStack {
+                            TeamView(team: item.teamsNormalized?.first, score: item.score?.scoreAway, line: item.lines?.first, isHome: false)
+                            Spacer().frame(height:16)
+                            TeamView(team: item.teamsNormalized?.last, score: item.score?.scoreHome, line: item.lines?.first, isHome: true)
+                            Spacer().frame(height:16)
+                            
+                            
+                        }.frame(width:proxy.size.width * 0.6)
                     Spacer()
-                    
-                    TeamView(team: item.teamsNormalized?.last, score: item.score?.scoreHome, line: item.lines?.first, isHome: true)
-                    
-                    // Customize this color
-                    
-                    if let message = item.message, !message.isEmpty {
-                        Text(message)
-                            .padding([.leading, .bottom], 12)
-                            .background(AssetNames.Colors.primaryColor) // Customize this color
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                    }
+                        VStack(alignment: .trailing) {
+                            Text(convertTo12HourFormat(item.score?.eventStatusDetail))
+                                .lineLimit(3)
+                                .truncationMode(.tail)
+                                .foregroundColor(.white)
+                            Text(item.score?.broadcast ?? "")
+                                .lineLimit(2)
+                                .truncationMode(.tail)
+                                .foregroundColor(.white)
+                        }.frame(width:proxy.size.width * 0.3)
                 }
-                .padding(12)
-                .background(AssetNames.Colors.primaryColor)
                 
-                Spacer()
-                
-                VStack(alignment: .trailing) {
-                    Text(convertTo12HourFormat(item.score?.eventStatusDetail))
-                        .lineLimit(3)
-                        .truncationMode(.tail)
-                    
-                    Text(item.score?.broadcast ?? "")
-                        .lineLimit(2)
-                        .truncationMode(.tail)
+                if let message = item.message, !message.isEmpty {
+                    Text(message)
+                        .foregroundColor(.white)
+                        .padding([.leading, .bottom], 12)
+                        .background(AssetNames.Colors.primaryColor) // Customize this color
+                        .frame(maxWidth: .infinity, alignment: .leading)
                 }
-                .padding(.trailing, 12)
-            }
+                
+            }.background(AssetNames.Colors.primaryColor).frame(height:180)
         }
     }
 }
-
 
 private func convertTo12HourFormat(_ time: String?) -> String {
     // Implement your time conversion logic here
     return time ?? ""
 }
-
 
 
