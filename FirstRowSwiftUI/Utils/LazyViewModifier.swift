295d723e54ce77310f72c980df43968bc125efec
marufkhan09 <maruf1609@gmail.com>
2024-08-12 19:19:40 +0600

    lazy modifier added

---
+++ FirstRowSwiftUI/Utils/LazyViewModifier.swift
@@ -0,0 +1,8 @@
+//
+//  LazyViewModifier.swift
+//  FirstRowSwiftUI
+//
+//  Created by Maruf Khan on 12/8/24.
+//
+
+import Foundation
---
+++ FirstRowSwiftUI/Views/Special/View/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  FirstRowSwiftUI
+//
+//  Created by Maruf Khan on 12/8/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
