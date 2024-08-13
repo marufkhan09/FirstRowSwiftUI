9bda80c7beb38c00d890b8fa9a2507d649ab65f3
marufkhan09 <maruf1609@gmail.com>
2024-08-13 13:24:11 +0600

    special detail done

---
+++ FirstRowSwiftUI/Views/Special/View/SpecialDetail.swift
@@ -0,0 +1,18 @@
+//
+//  SpecialDetail.swift
+//  FirstRowSwiftUI
+//
+//  Created by Maruf Khan on 13/8/24.
+//
+
+import SwiftUI
+
+struct SpecialDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpecialDetail()
+}
